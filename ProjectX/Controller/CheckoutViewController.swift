739f22370bb27050fe52821ae03be7c443533413
Nikolas Omelianov <nikolas@Nikolass-Mac-mini.local>
2017-12-02 20:30:45 +0200

    first look

---
+++ ProjectX/Controller/CheckoutViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectX
+//
+//  Created by Nikolas Omelianov on 02.12.17.
+//  Copyright © 2017 Nikolas Omelianov. All rights reserved.
+//
+
+import UIKit
+
+class CheckoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
